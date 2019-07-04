6ac6861e273ddb54acde0ab1ecd9e76b515eb3f1
heguopei <2386824052@qq.com>
2019-07-04 22:42:19 +0800

    添加字体

--- src/main/java/com/hegp/controller/CaptchaController.java
+++ src/main/java/com/hegp/controller/CaptchaController.java
@@ -1,36 +1,55 @@
 package com.hegp.controller;
 
-import com.hegp.core.captcha.Captcha;
-import com.hegp.core.captcha.GifCaptcha;
+import com.hegp.utils.ResourcesUtils;
+import com.wf.captcha.GifCaptcha;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+//import java.awt.Font;
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 @RestController
 public class CaptchaController {
+    private Font font;
+
+    @Value("${font.font.path:classpath:config/UbuntuMonoRegular.ttf}")
+    private String fontFilePath;
+
+    @PostConstruct
+    public void init() throws IOException, FontFormatException {
+        File file = ResourcesUtils.getFileByPath(fontFilePath);
+        font = Font.createFont(Font.TRUETYPE_FONT, file);
+        font = font.deriveFont(Font.PLAIN+Font.ITALIC, 25);
+    }
+
     /**
      * 获取验证码（Gif版本）
      * @param response
      */
     @GetMapping(value="/captcha")
-    public void getGifCode(HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public void getGifCode1(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        // 设置请求头为输出图片类型
+        response.setContentType("image/gif");
         response.setHeader("Pragma", "No-cache");
         response.setHeader("Cache-Control", "no-cache");
-        response.setDateHeader("Expires", 0);
-        response.setContentType("image/gif");
-        /**
-         * gif格式动画验证码
-         * 宽，高，位数。
-         */
-        Captcha captcha = new GifCaptcha(146,33,4);
+        response.setDateHeader("Expires", 0L);
+
+        // 三个参数分别为宽、高、位数
+        GifCaptcha gifCaptcha = new GifCaptcha(120, 40, 4);
+
+        gifCaptcha.setFont(font);  // 有默认字体，可以不用设置
         HttpSession session = request.getSession(true);
-        //输出
-        captcha.out(response.getOutputStream());
-        //存入Session
-        session.setAttribute("_code",captcha.text().toLowerCase());
+        gifCaptcha.out(response.getOutputStream());
+
+        // 验证码存入session
+        session.setAttribute("captcha", gifCaptcha.text().toLowerCase());
     }
 }
