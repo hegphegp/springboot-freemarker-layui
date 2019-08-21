d604d80add835f4117ffd18f8b1a090d5842cc3b
heguopei <2386824052@qq.com>
2019-08-21 23:48:54 +0800

    SimpleJpaRepository对象

--- src/main/java/com/hegp/core/jpa/service/impl/JPAServiceImpl.java
+++ src/main/java/com/hegp/core/jpa/service/impl/JPAServiceImpl.java
@@ -1,19 +1,25 @@
 package com.hegp.core.jpa.service.impl;
 
+import com.hegp.core.config.InitJpaRepositoryConfig;
 import com.hegp.core.jpa.service.JPAService;
 import com.hegp.core.jpa.service.Query;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import javax.persistence.EntityManager;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 public class JPAServiceImpl<T, ID> implements JPAService<T, ID>, InitializingBean, ApplicationContextAware {
+    public SimpleJpaRepository<T, ID> simpleJpaRepository;
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
+        Class<T> dc = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+        System.out.println("\n\n\n\n\n\n\n\n\n"+dc.getClass()+"\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        simpleJpaRepository = InitJpaRepositoryConfig.simpleJpaRepositoryMap.get(dc);
     }
 
     @Override
