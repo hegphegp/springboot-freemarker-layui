86642bf3b17a3423ec0bdf30622563c9f6a4a65b
heguopei <heguopei@ascs.tech>
2019-08-21 08:59:45 +0800

    动态拼接根路径，不拼接schema协议和域名端口

--- src/main/java/com/hegp/core/filter/BasePathConfigFilter.java
+++ src/main/java/com/hegp/core/filter/BasePathConfigFilter.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -28,34 +26,30 @@ public class BasePathConfigFilter extends OncePerRequestFilter {
          * zuul网关会自动封装 x-forwarded-host 参数
          * zuul网关不会自动封装 x-forwarded-uri 参数,要手动写代码补上去
          */
-        String contextPath = request.getContextPath();
-        String basePath = StringUtils.hasText(request.getHeader("x-forwarded-host"))? assemblyWhenXForwardedHostExists(request):"";
-        basePath += StringUtils.hasText(contextPath)? contextPath:"";
+        String basePath = StringUtils.hasText(request.getHeader("x-forwarded-host"))? assemblyBasePathWhenRequestIsForwarded(request):"";
+        basePath += StringUtils.hasText(request.getContextPath())? request.getContextPath():"";
         request.setAttribute("basePath", basePath);
         // 如果用了 nginx作为请求入口，一定要配置
         /**
-         *  proxy_set_header X-Real-IP $remote_addr;
-         *  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-         *  proxy_set_header X-Forwarded-Proto $scheme;
          *  proxy_set_header Host $http_host;
          *  proxy_set_header X-Forwarded-Uri $uri;
+         *  proxy_set_header X-Real-IP $remote_addr;
+         *  proxy_set_header X-Forwarded-Proto $scheme;
+         *  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
          */
         printAllHeaders(request);
         filterChain.doFilter(request, response);
     }
 
-    private String assemblyWhenXForwardedHostExists(HttpServletRequest request) {
+    private String assemblyBasePathWhenRequestIsForwarded(HttpServletRequest request) {
         String xForwardedUri = request.getHeader("x-forwarded-uri");
         Assert.isTrue(StringUtils.hasText(xForwardedUri), "请配置请求头的 x-forwarded-uri 参数");
         String requestURI = request.getRequestURI();
-        String dynamicPath = null;
         if ("/".equals(requestURI)) {
-            dynamicPath = xForwardedUri.endsWith("/")? xForwardedUri.substring(0, xForwardedUri.length()-1):xForwardedUri;
+            return xForwardedUri.endsWith("/")? xForwardedUri.substring(0, xForwardedUri.length()-1):xForwardedUri;
         } else {
-            dynamicPath = xForwardedUri.substring(0, xForwardedUri.lastIndexOf(requestURI));
+            return xForwardedUri.substring(0, xForwardedUri.lastIndexOf(requestURI));
         }
-        String scheme = StringUtils.hasText(request.getScheme())? request.getScheme():"http";
-        return scheme+"://"+request.getHeader("x-forwarded-host")+dynamicPath;
     }
 
     public void printAllHeaders(HttpServletRequest request) {
