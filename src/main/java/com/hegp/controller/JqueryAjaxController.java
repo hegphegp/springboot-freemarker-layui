eedf5e1bae7b262ef7dbdc83e6d58291cc80e568
heguopei <2386824052@qq.com>
2019-08-25 21:51:47 +0800

    添加ajax上传post的json请求

--- src/main/java/com/hegp/controller/JqueryAjaxController.java
+++ src/main/java/com/hegp/controller/JqueryAjaxController.java
@@ -9,6 +9,15 @@ import java.util.Map;
 @RestController
 @RequestMapping("/v1/jquery-ajax")
 public class JqueryAjaxController {
+    @PostMapping(value = "/request-body")
+    public RequestResponse testRequestBody(@RequestBody Map params) {
+        Map map = new HashMap();
+        map.put("username", params.get("username"));
+        map.put("password", params.get("password"));
+        System.out.println(map);
+        return RequestResponse.build(map);
+    }
+
     @PostMapping(value = "/x-www-form-urlencoded")
     public RequestResponse xWwwFormUrlencoded(@RequestParam(name="username", required = false) String username,
                                               @RequestParam(name="password", required = false) String password) {
